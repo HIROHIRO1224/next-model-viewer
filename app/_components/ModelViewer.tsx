656dd006a152d27ed46f79b1ffae818c26479092
HIROHIRO1224 <2001hiroto.yoshikawa1224@gmail.com>
2023-09-28 06:31:04 +0000

    init commit

---
+++ app/_components/ModelViewer.tsx
@@ -0,0 +1,41 @@
+"use client";
+import "@google/model-viewer";
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "model-viewer": MyElementAttributes;
+    }
+    interface MyElementAttributes {
+      src: string;
+      poster: string;
+      alt: string;
+      ar: boolean;
+      scale: string;
+    }
+  }
+}
+
+type Props = {
+  poster: string;
+  glb: string;
+  usdz: string;
+  alt: string;
+  scale?: number;
+  height?: string;
+};
+export const ModelViewer = (props: Props) => {
+  const { poster, glb, usdz, alt, scale = 1, height = "80vh" } = props;
+
+  return (
+    <model-viewer
+      src={glb}
+      ios-src={usdz}
+      poster={poster}
+      alt={alt}
+      shadow-intensity="1"
+      camera-controls
+      ar={true}
+      scale={`${scale} ${scale} ${scale}`}
+    ></model-viewer>
+  );
+};
